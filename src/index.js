efb3e0f74f910b86c59f7b77e3ed2cbd524872af
Tomas Hipolito <tlhipolito67@gmail.com>
2017-09-14 10:20:56 -0500

    started to add the yes and no buttons to the html

--- src/index.js
+++ src/index.js
@@ -1,20 +1,33 @@
-/**
- * es6 modules and imports
- */
-// import sayHello from './hello';
-// sayHello('World');
+/* es6 modules and imports*/
 
+import sayHello from './hello';
+import $ from "jquery";
+sayHello('World');
+import getLoadMsg from "./loading";
+
+
+$('#inputs').hide();
+$(".container").text(`${getLoadMsg()}`);
 /**
  * require style imports
  */
-//
 const getMovies = require('./getMovies.js');
+
+addEventListener(){
+
 getMovies().then((movies) => {
-  console.log('Here are all the movies:');
-  movies.forEach(({title, rating, id}) => {
-    console.log(`id#${id} - ${title} - rating: ${rating}`);
-  });
+    // console.log('Here are all the movies:');
+    let movie = "";
+    movies.forEach(({title, rating, id}) => {
+        movie += (`id#${id} - ${title} - rating: ${rating}`);
+    });
+
+    $(".container").html('').append(movie);
+    $('#inputs').show();
+
+
 }).catch((error) => {
-  alert('Oh no! Something went wrong.\nCheck the console for details.')
-  console.log(error);
+    alert('Oh no! Something went wrong.\nCheck the console for details.');
+    console.log(error);
 });
+}
---
+++ src/loading.js
@@ -0,0 +1,15 @@
+const getLoadMsg = () => {
+    return "Loading";
+
+};
+
+module.exports = getLoadMsg;
+
+var yes = (true)
+var no = (false)
+
+if (yes){
+    return document.getElementById(["1","2","3","4"]())
+}else{
+    alert ("Fine Be That way")
+};
